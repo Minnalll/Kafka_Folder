5372a2e479b6b378db1218afbe30e4379c29c66f
Minnalll <ukumar6@deloitte.com>
2024-07-14 22:45:35 +0530

    Kafka Reference

---
+++ springboot-kafka-tutorial/src/main/java/com/springboot/kafka/json/springboot_kafka_tutorial/controller/JsonMessageController.java
@@ -0,0 +1,29 @@
+package com.springboot.kafka.json.springboot_kafka_tutorial.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.kafka.json.springboot_kafka_tutorial.entity.User;
+import com.springboot.kafka.json.springboot_kafka_tutorial.kafka.JsonKafkaProducer;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+	
+    private JsonKafkaProducer kafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+}
+
---
+++ springboot-kafka-tutorial/src/main/java/com/springboot/kafka/json/springboot_kafka_tutorial/entity/User.java
@@ -0,0 +1,16 @@
+package com.springboot.kafka.json.springboot_kafka_tutorial.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+	
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
